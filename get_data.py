4fea638411dccff7577459ccb4ae7ca400db14a3
Aritra Biswas <pandalearnstocode@gmail.com>
2021-03-21 15:23:52 +0530

    adding dvc remote with azure blob.

--- get_data.py
+++ get_data.py
@@ -6,7 +6,7 @@ from sklearn.datasets import make_regression
 
 # If there's no dataset in the project directory, create a reasonably large one.
 # If it exists, append some new observations.
-if os.path.isfile("data.csv"):
+if os.path.isfile("data.csv.gzip"):
     n = 1
 else:
     n = 50
@@ -14,4 +14,4 @@ else:
 for i in range(0, n):
     X, y = make_regression(10000, n_features=10)
     df = pd.DataFrame(X)
-    df.to_csv("data.csv", mode="a")
+    df.to_csv("data.csv.gzip", mode="a", index=None, compression="gzip")
